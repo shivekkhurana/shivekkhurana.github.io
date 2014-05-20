80e3ec7eebb03a0d13e464ae7a3f1a436d8e72f0
Shivek Khurana <khuranashivek@gmail.com>
2014-05-20 11:01:54 +0530

    Much todo done. WOW.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 #TODO
 
 - <strike>find a away to make name more visible</strike>
-- set Loader for bg image
+- <strike>preload bg</strike>
 - <strike>make links work</strike>
 - give credit to photographer
-- make mobile ready
+- <strike>make mobile ready</strike>
 - your own picture 
 - <strike>get rid of blue base</strike>
-- meta tags and seo
+- <strike>meta tags and seo</strike>
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Code goes here
-
 $(function() {
-  $('.name').addClass('animated bounceInUp');
-  $('.contact').addClass('animated bounceInDown')
+	$('<img/>').attr('src', 'bg.jpg').load(function() {
+		$(this).remove(); // prevent memory leaks as @benweet suggested
+		$('body').css('background-image', 'url(bg.jpg)');
+		$('.name').addClass('animated bounceInUp');
+		$('.contact').addClass('animated bounceInDown')
+	});
 });
